dc4b9b150042fb8eedf26848904b4c3cf3c61733
tnayanam <nayatanuj@gmail.com>
2017-04-01 15:21:41 -0400

    Addition of Gig Form

---
+++ MyMusic/Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using MyMusic.Models;
+using MyMusic.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MyMusic.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // Create
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ MyMusic/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MyMusic.Models;
+using System.Collections.Generic;
+
+namespace MyMusic.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
