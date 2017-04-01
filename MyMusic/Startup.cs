97baa4f03728585c77dd5f663d8fbb8b218e9601
tnayanam <nayatanuj@gmail.com>
2017-03-31 22:03:51 -0400

    Initial Push

---
+++ MyMusic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMusic.Startup))]
+namespace MyMusic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
