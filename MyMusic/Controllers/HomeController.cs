59d94a8cbcf725ecdd4cc7482f13fb279a602621
tnayanam <nayatanuj@gmail.com>
2017-04-02 12:18:28 -0400

    Addng absolute and relative position

--- MyMusic/Controllers/HomeController.cs
+++ MyMusic/Controllers/HomeController.cs
@@ -16,8 +16,9 @@ namespace MyMusic.Controllers
         }
         public ActionResult Index()
         {
-            var upcomingGigs = _context.Gigs.
-                                Include(g => g.Artist)
+            var upcomingGigs = _context.Gigs
+                                .Include(g => g.Artist)
+                                .Include(g=>g.Genre)
                                 .Where(g => g.DateTime > DateTime.Now);
 
             return View(upcomingGigs);
