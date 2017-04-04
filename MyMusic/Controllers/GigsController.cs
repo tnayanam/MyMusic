0fb3b527b9b755adda7adce190f54a04bb968c8e
tnayanam <nayatanuj@gmail.com>
2017-04-03 23:46:00 -0400

    Editing and Updating the gig

--- MyMusic/Controllers/GigsController.cs
+++ MyMusic/Controllers/GigsController.cs
@@ -97,10 +97,30 @@ namespace MyMusic.Controllers
                 Date = gig.DateTime.ToString("d MMM yyyy"),
                 Time = gig.DateTime.ToString("HH:mm"),
                 Genre = gig.GenreId,
-                Heading = "Edit a Gig"
+                Heading = "Edit a Gig",
+                Id = gig.Id
             };
 
             return View("GigForm", viewModel);
+        }
+
+        [Authorize]
+        public ActionResult Update(GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("GigForm", viewModel);
+            }
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs
+                .Single(g => g.Id == viewModel.Id && g.ArtistId == userId);
+            gig.Venue = viewModel.Venue;
+            gig.DateTime = viewModel.GetDateTime();
+            gig.GenreId = viewModel.Genre;
+
+            _context.SaveChanges();
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }
--- MyMusic/ViewModels/GigFormViewModel.cs
+++ MyMusic/ViewModels/GigFormViewModel.cs
@@ -8,6 +8,8 @@ namespace MyMusic.ViewModels
 {
     public class GigFormViewModel
     {
+        public int Id { get; set; }
+
         [Required]
         public string Venue { get; set; }
 
@@ -23,6 +25,14 @@ namespace MyMusic.ViewModels
         public byte Genre { get; set; }
 
         public string Heading { get; set; }
+
+        public string Action
+        {
+            get
+            {
+                return (Id != 0) ? "Update" : "Create";
+            }
+        }
 
         public DateTime GetDateTime()
         {
